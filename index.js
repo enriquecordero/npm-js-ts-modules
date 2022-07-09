8bc8e791102c029979e7f05ce21d48c161984ffd
Enrique Cordero <ecs5000@gmail.com>
2022-07-09 18:07:45 -0400

    Modulos sencillos

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  hola: function() {
+    console.log("Hola Mundo")
+  },
+
+  holaPersonalizado: function(nombre) {
+    console.log(`Hola Mundo ${nombre}`)
+  },
+
+  holaCursoNPM: function() {
+    console.log(`Hola al curso de NPM`)
+  }
+}
